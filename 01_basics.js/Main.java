4e02e311db509f4bee613bdb0247b7d857c8eb49
harsh <Harsh24235@iiitd.ac.in>
2025-08-05 09:18:10 +0000

    basics

---
+++ 01_basics.js/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner ; 
+
+public class Main{
+    public static void main(String[] args ){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("hello! what is your name : ");
+        String name  = sc.nextLine();
+        System.out.println("hello "+name+" you have a nce day");
+
+    }
+}
